02df8e583537f3343df05d26fa16ab9e620895ac
jagranados <tekem.o.matic@gmail.com>
2018-02-12 21:28:19 +0100

    Primer deploy

--- omnibus-nrpe/config/software/nrpe.rb
+++ omnibus-nrpe/config/software/nrpe.rb
@@ -1,7 +1,7 @@
 
 name "nrpe"
 default_version "3.2.1"
-source url: "https://github.com/NagiosEnterprises/nrpe/releases/download/nrpe-#{version}/nrpe-#{version}.tar.gz"
+source url: "https://github.com/NagiosEnterprises/nrpe/releases/download/nrpe-#{version}/nrpe-#{version}.tar.gz",
        md5: "8997e195fea93cdceb8c7ed8ac1d43bc"
 
 dependency "zlib"
